7ec1d877e950d41c17a4daf09c58a617c849790a
DarioneNazionale <56448497+DarioneNazionale@users.noreply.github.com>
2019-12-01 17:34:08 +0100

    clustering library, working and ready.

--- clustering_lib.py
+++ clustering_lib.py
@@ -12,6 +12,63 @@ def dist(v1, v2):
     distance = math.sqrt(sum([(a - b) ** 2 for a, b in zip(v1, v2)])) 
     return distance
 
+
+
+def plotting(centroids, classes, dimensions_number):
+    # ---------------------------------------- plotting --------------------------------------------------------------
+    # naming the features, so we can add them to the plots
+    features = ["Alcohol",
+                "Malic acid",
+                "Ash",
+                "Alcalinity",
+                "Magnesium",
+                "Total phenols",
+                "Flavanoids",
+                "Nonflavanoid",
+                "Proanthocyanins",
+                "Color",
+                "Hue",
+                "diluted wines",
+                "Proline"]
+
+    # findingout all the possible cupples of dimensions
+    combinations_of_dimension = list(itertools.combinations([dim for dim in range(dimensions_number)], 2))
+
+    for i in range(len(combinations_of_dimension)):  # building a subplot for each cupple of dimensions
+
+        if i % 4 == 0:  # create a new figure for each 4 plot
+            fig, axes = plt.subplots(nrows=1, ncols=4)
+            fig.set_size_inches(18.5, 4, forward=True)
+
+        #definging the dimensions
+        dim1, dim2 = combinations_of_dimension[i]
+
+        # creating one of the plots
+        all_centroids = dd(list)
+        for cluster in classes.keys():  # iterating on each cluster
+            # saving the cluster cordinates
+            all_centroids["dim1"].append(centroids[cluster][dim1])
+            all_centroids["dim2"].append(centroids[cluster][dim2])
+
+            # get the codinates of all the cluster points
+            points = {"dim1": [point[dim1] for point in classes[cluster]],
+                      "dim2": [point[dim2] for point in classes[cluster]]}
+
+            axes[i % 4].scatter(points["dim1"], points["dim2"])  # adding the cluster points
+
+        # add the centroids to the plot
+        axes[i % 4].scatter(all_centroids["dim1"], all_centroids["dim2"], marker='x')
+
+        # adding lables to the plots
+        axes[i % 4].set_xlabel(features[dim1])
+        axes[i % 4].set_ylabel(features[dim2])
+        # removing axis
+        axes[i % 4].set_xticklabels([])
+        axes[i % 4].set_yticklabels([])
+
+    #ploting everithing.
+    plt.show()
+
 def kmeansOnWines(k):
 
     #getting the data set with pandas
@@ -45,52 +102,4 @@ def kmeansOnWines(k):
         if all([np.allclose(x, y) for x, y in zip(centroids, old_centroids)]): #if the previous centroids are equal to the new ones
             go_on = False #break the loop
 
-
-    #---------------------------------------- plotting --------------------------------------------------------------
-    # naming the features, so we can add them to the plots
-    features = ["Alcohol",
-                "Malic acid",
-                "Ash",
-                "Alcalinity",
-                "Magnesium",
-                "Total phenols",
-                "Flavanoids",
-                "Nonflavanoid",
-                "Proanthocyanins",
-                "Color",
-                "Hue",
-                "diluted wines",
-                "Proline"]
-
-    #findingout all the possible cupples of dimensions
-    combinations_of_dimension = list(itertools.combinations([dim for dim in range(len(data[0]))], 2))
-
-    fig = plt.figure(figsize=(15,15)) #setting up the figure that will host all the subplots
-    i = 1 #starting on the first position of the figure
-    for dim1, dim2 in combinations_of_dimension: #building a subplot for each cupple of dimensions
-        #chreating the subplot
-        ax = fig.add_subplot(math.ceil(len(combinations_of_dimension)/10), 10, i)
-        # adding lables to the plots
-        plt.xlabel(features[dim1])
-        plt.ylabel(features[dim2])
-
-        #creating one of the plots
-        all_centroids = dd(list)
-        for cluster in classes.keys(): #iterating on each cluster
-            #saving the cluster cordinates
-            all_centroids["dim1"].append(centroids[cluster][dim1])
-            all_centroids["dim2"].append(centroids[cluster][dim2])
-
-            points = dd(list) #get the codinates of all the cluster points
-            points["dim1"] = [point[dim1] for point in classes[cluster]]
-            points["dim2"] = [point[dim2] for point in classes[cluster]]
-
-            plt.scatter(points["dim1"], points["dim2"]) #adding the cluster points
-
-        #add the centroids to the plot
-        plt.scatter(all_centroids["dim1"], all_centroids["dim2"], marker='x')
-
-        i += 1 #moving to the next plot
-
-    #now we have all the plots
-    plt.show()
+    plotting(centroids, classes, len(data[0])) #ploting the clusters
